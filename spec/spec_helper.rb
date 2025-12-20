067bd3844bf0f6415bf3b17d013f609f21577227
Paul Shields <noisyoly@gmail.com>
2025-12-20 16:49:58 +0000

    Force IPv4 127.0.0.1 for RSpec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,5 @@ RSpec.configure do |config|
   
   # Base URL of the running Python application
   # CircleCI or local can set APP_URL if different
-  config.add_setting :base_url, default: ENV['APP_URL'] || 'http://localhost:5000'
+  config.add_setting :base_url, default: ENV['APP_URL'] || 'http://127.0.0.1:5000'
 end
